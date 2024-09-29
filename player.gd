4bb37979df1e7d9cffa193d60ccfcb256912d19e
erpw <erpw@unc.edu>
2024-09-29 00:31:14 -0400

    Basic tilemap and player movement
    Set up basic player controller with movement and a top-down tilemap. Please excuse the programmer art!

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed := 50
+@export var friction := 0.1
+@export var acceleration := 0.1
+
+
+func _physics_process(_delta):
+	var movement_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	if movement_vector.length() > 0:
+		velocity = lerp(velocity, movement_vector * speed, acceleration)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, friction)
+	move_and_slide()
