e499c62f8a10c8d3e528a287ee88bb034aa73dc4
erpw <erpw@unc.edu>
2024-10-02 19:32:10 -0400

    fix bug :)
    manually hide all interactable labels on exit in addition to enter to avoid bug where spamming interact can show multiple sign labels at once

--- interactable.gd
+++ interactable.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 	if label != null:
 		label.text = "[E] to " + action_name
 
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.interactables != null:
 		for e in area.interactables:
@@ -20,6 +21,12 @@ func _on_area_entered(area: Area2D) -> void:
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.interactables != null:
+		# yes, this does need to be in both functions
+		# to avoid bug:
+		# spamming interact can eg make 2 signs show at once
+		for e in area.interactables:
+			e.interactable.label.hide()
+			e.end_interaction()
 		area.interactables.erase(get_parent())
 		label.hide()
 		get_parent().end_interaction()
